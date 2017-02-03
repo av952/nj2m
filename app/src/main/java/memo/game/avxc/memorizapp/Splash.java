80ba8f5d4ce1541b41bad5bc3583ef45b7bc876c
av952 <alejandrov952@hotmail.com>
2017-02-03 15:21:42 -0500

    splash

--- app/src/main/java/memo/game/avxc/memorizapp/Splash.java
+++ app/src/main/java/memo/game/avxc/memorizapp/Splash.java
@@ -1,15 +1,39 @@
 package memo.game.avxc.memorizapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
 
 public class Splash extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 2000;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        handler();
+
     }
+
+    public void handler(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, android.view.Menu.class);
+                startActivity(intent);
+                    finish();
+
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+
+
 }
