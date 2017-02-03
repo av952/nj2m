00e4f24db7da4c2443733ad167bbfdfc9608e3fe
av952 <alejandrov952@hotmail.com>
2017-02-03 16:44:23 -0500

    cree las actividades par cada nivel

---
+++ app/src/main/java/memo/game/avxc/memorizapp/Nivel_medio.java
@@ -0,0 +1,13 @@
+package memo.game.avxc.memorizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nivel_medio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nivel_medio);
+    }
+}
